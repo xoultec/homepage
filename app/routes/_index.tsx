3abc69e7c71112b358e34ca9328eadc38304adc5
Ruben Nunez <ruben_abdias@live.com>
2024-02-12 09:46:42 -0600

    Upload preliminary website

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,23 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "XoulTec" },
+    { name: "description", content: "Welcome to XoulTec!" },
   ];
 };
 
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>Welcome to XoulTec</h1>
+      <p>Homepage is under construction.</p>
+      <p>
+        If you are looking to pay your bill, please visit our{" "}
+        <a href="https://pay.xoultec.com/p/login/6oE8xf7s9bKQ1xKaEE">
+          customer portal
+        </a>
+        .
+      </p>
     </div>
   );
 }
